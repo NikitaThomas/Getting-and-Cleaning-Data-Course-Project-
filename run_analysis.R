f53a58adfa7b0edc8a241e361ad9d70a2c9e085f
Nikita Thomas <nikita1103@gmail.com>
2016-04-18 16:58:02 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,2 +1,11 @@
 library(data.table)
 library(dplyr)
+
+//Supporting Metadata
+feature_Names <- read.table("Desktop/UCI HAR Dataset/features.txt")
+activity_Labels <- read.table("UCI HAR Dataset/activity_labels.txt", header = FALSE)
+
+//Training Data
+subject_Train <- read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+activity_Train <- read.table("UCI HAR Dataset/train/y_train.txt", header = FALSE)
+features_Train <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
